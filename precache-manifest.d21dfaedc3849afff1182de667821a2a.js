6b51424717cb34a55fa3ea45cac0e1ccda2e2dda
pranu1993 <pranupranuankit@gmail.com>
2022-08-21 14:05:11 +0530

    Updates

---
+++ precache-manifest.d21dfaedc3849afff1182de667821a2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e938858c2f45c15a03ac",
+    "url": "/pomodro/static/css/main.1f5ea5a3.chunk.css"
+  },
+  {
+    "revision": "e938858c2f45c15a03ac",
+    "url": "/pomodro/static/js/main.e938858c.chunk.js"
+  },
+  {
+    "revision": "73d3c09f02479628f9ef",
+    "url": "/pomodro/static/js/1.73d3c09f.chunk.js"
+  },
+  {
+    "revision": "cc483d10b9d3f017737c",
+    "url": "/pomodro/static/js/runtime~main.cc483d10.js"
+  },
+  {
+    "revision": "78ac40d5998e4efdb2f97b68330bae40",
+    "url": "/pomodro/index.html"
+  }
+];
